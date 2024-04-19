7fe4e1b1f7feac3962dfa4a80f715b060b30c138
nhshieng <sliu@nhgh.org>
2024-04-19 15:38:11 -0700

    basic functions tested

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import serial
 import struct
 
-READ_HOLDING_REGISTERS = 0x03
-WRITE_SINGLE_REGISTER = 0x06
+READ_REGISTER = 0x03
+WRITE_REGISTER = 0x06
 SLAVE_ADDRESS = 1
 
 SERIAL_PORT = '/dev/ttyACM0'
@@ -24,41 +24,69 @@ def calculate_crc(data):
 	return struct.pack('<H', crc)
 
 
-def construct_request(function_code, starting_address, quantity):
-	return bytes([SLAVE_ADDRESS, function_code]) + \
-		struct.pack('>HH', starting_address, quantity) + \
-		calculate_crc(bytes([SLAVE_ADDRESS, function_code, starting_address >> 8, starting_address & 0xFF, quantity >> 8, quantity & 0xFF]))
+def construct_request(read_write, register_address, value):
+	if read_write == 'read':
+		read_write = READ_REGISTER
+	elif read_write == 'write':
+		read_write = WRITE_REGISTER
 
-
-def construct_write_request(register_address, value):
-	return bytes([SLAVE_ADDRESS, WRITE_SINGLE_REGISTER]) + \
+	return bytes([SLAVE_ADDRESS, read_write]) + \
 		struct.pack('>HH', register_address, value) + \
-		calculate_crc(bytes([SLAVE_ADDRESS, WRITE_SINGLE_REGISTER, register_address >> 8, register_address & 0xFF, value >> 8, value & 0xFF]))
+		calculate_crc(bytes([SLAVE_ADDRESS, read_write, register_address >> 8, register_address & 0xFF, value >> 8, value & 0xFF]))
+
+
+def set_temp(temp):
+	temp_sent = temp * 10
+	request = construct_request('write', 0, temp_sent)
+	ser.write(request)
+
+
+def read_temp():
+	request = construct_request('read', 1, 1)
+	ser.write(request)
+	response = ser.read(7)
+
+	if len(response) == 7:
+		data_bytes = response[3:5]
+		value = struct.unpack('>h', data_bytes)[0]
+		temp = value / 10.0
+		return temp
+	else:
+		print ("invalid length")
+
+
+def read_set_temp():
+	request = construct_request('read', 0, 1)
+	ser.write(request)
+	response = ser.read(7)
+
+	if len(response) == 7:
+		data_bytes = response[3:5]
+		value = struct.unpack('>h', data_bytes)[0]
+		temp = value / 10.0
+		return temp
+	else:
+		print ("invalid length")
+	
 
 
 try:
 	ser = serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=TIMEOUT)
 
-	#request = construct_request(READ_HOLDING_REGISTERS, 0, 1)
+	#request = construct_request(0, 1)
+	#register_address = 0
+	#value_to_write = 2 * 10
+
+	#request = construct_request('read', register_address, 1)
 	#ser.write(request)
-	#response = ser.read(7)
-
-	#if len(response) == 7:
-	#	data_bytes = response[3:5]
-	#	value = struct.unpack('>h', data_bytes)[0]
-	#	setpoint = value / 10.0
-	#	print("Response: ", setpoint)
-
-	#else:
-	#	print("invalid length")
-
 	
-	register_address = 0
-	value_to_write = 8 * 10
+	#response = ser.read(7)
+	#data_bytes = response[3:5]
+	#value = struct.unpack('>h', data_bytes)[0]
+	#setpoint = value / 10.0
+	#print (setpoint)
 
-	request = construct_write_request(register_address, value_to_write)
-	ser.write(request)
-
+	print (read_set_temp())
 	ser.close()
 	
 except KeyboardInterrupt:
