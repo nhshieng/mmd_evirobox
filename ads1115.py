08712feda1ee99408f246e6f67acdd3536b60dbd
Shieng Liu <sliu@nhgh.org>
2024-09-13 11:00:39 -0700

    ready to test

--- ads1115.py
+++ ads1115.py
@@ -4,11 +4,18 @@ import board
 import adafruit_ads1x15.ads1115 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
 
-i2c = busio.I2C(board.SCL, board.SDA)
+def i2c_setup():
+	i2c = busio.I2C(board.SCL, board.SDA)
+	ads_1 = ADS.ADS1115(i2c, address=0x48, gain=2)
+	ads_2 = ADS.ADS1115(i2c, address=0x49)
+	return ads_1, ads_2
 
-ads_1 = ADS.ADS1115(i2c, address=0x48, gain=2)
-ads_2 = ADS.ADS1115(i2c, address=0x49)
-
+def port(number):
+	if number == 0:
+		return ADS.P0
+	elif number == 1:
+		return ADS.P1
+	
 channel_1 = AnalogIn(ads_1, ADS.P0)
 channel_2 = AnalogIn(ads_1, ADS.P1)
 channel_3 = AnalogIn(ads_1, ADS.P2)
@@ -18,8 +25,41 @@ channel_6 = AnalogIn(ads_2, ADS.P1)
 channel_7 = AnalogIn(ads_2, ADS.P2)
 channel_8 = AnalogIn(ads_2, ADS.P3)
 
+channels = [channel_1, channel_2, channel_3, channel_4, channel_5, channel_6, channel_7, channel_8]
 
-while True:
-	print(channel_1.value, "\t", channel_2.value, "\t", channel_5.value) 
+class Sensor:
+	def __init__(self, ads, port):
+		self.ads = ads
+		self.port = port
+	
+	def setup(self):
+		gas_sensor = AnalogIn(self.ads, self.port)
+		return (gas_sensor)
 
-	time.sleep(5)
+def tare_sensors():
+	reading = read_sensors()
+
+	with open('tare.txt', 'w') as file:
+		for value in reading:
+			file.write(f"{value}]\n")
+
+def read_tare_values():
+	with open('tare.txt', 'r') as file:
+		tare_values = file.readlines()
+		tare_values = [int(number.strip()) for number in tare_values]
+	return tare_values
+
+def read_sensors(readings, time):
+	read_running_total = [] * len(channels)
+	read_counter = 0
+
+	while read_counter < readings:
+		channel_counter = 0
+		for channel in channels:
+			read_running_total[channel_counter] += channel.value
+			channel_counter += 1	
+		read_counter += 1
+		time.sleep(time)
+
+	averaged_read_values = [x / readings for x in read_running_total]
+	return averaged_read_values
--- main.py
+++ main.py
@@ -1,118 +1,16 @@
+from ads1115 import i2c_setup, Sensor, tare_sensors, read_tare_values, read_sensors, port
+
 import serial
 import struct
-import time
 
-READ_REGISTER = 0x03
-WRITE_REGISTER = 0x06
-SLAVE_ADDRESS = 1
+ads_1, ads_2 = i2c_setup()
+gas_1 = Sensor(ads_1, port(0)).setup()
 
-SERIAL_PORT = '/dev/ttyACM0'
-BAUD_RATE = 115200
-TIMEOUT = 1
-
-
-def calculate_crc(data):
-	crc = 0xFFFF
-	for byte in data:
-		crc ^= byte
-		for _ in range(8):
-			if crc & 0x0001:
-				crc >>= 1
-				crc ^= 0xA001
-			else:
-				crc >>= 1
-
-	return struct.pack('<H', crc)
-
-
-def construct_request(read_write, register_address, value):
-	if read_write == 'read':
-		read_write = READ_REGISTER
-	elif read_write == 'write':
-		read_write = WRITE_REGISTER
-
-	return bytes([SLAVE_ADDRESS, read_write]) + \
-		struct.pack('>HH', register_address, value) + \
-		calculate_crc(bytes([SLAVE_ADDRESS, read_write, register_address >> 8, register_address & 0xFF, value >> 8, value & 0xFF]))
-
-
-def set_temp(temp):
-	temp_sent = temp * 10
-	request = construct_request('write', 0, temp_sent)
-	ser.write(request)
-
-
-def read_temp():
-	request = construct_request('read', 1, 1)
-	ser.write(request)
-	response = ser.read(7)
-
-	if len(response) == 7:
-		data_bytes = response[3:5]
-		value = struct.unpack('>h', data_bytes)[0]
-		temp = value / 10.0
-		return temp
-	else:
-		print ("invalid length")
-
-
-def read_set_temp():
-	request = construct_request('read', 0, 1)
-	ser.write(request)
-	response = ser.read(7)
-
-	if len(response) == 7:
-		data_bytes = response[3:5]
-		value = struct.unpack('>h', data_bytes)[0]
-		temp = value / 10.0
-		return temp
-	else:
-		print ("invalid length")
+print(gas_1.value)
+#try:
+#	ser = serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=TIMEOUT)
+#	temp_ramp_test(3600)
+#	ser.close()
 	
-
-def temp_ramp_test(dwell_time):
-	print ('Beginning Temperature Ramp Testing')
-
-	testing_temps = [27, 30, 35, 37, 40]
-	for t in testing_temps:
-		print ('Setting Temp To:', t)
-		set_temp(t)
-		time.sleep(1)
-		ser.flushInput()
-
-		while abs(read_set_temp() - read_temp()) > 0.5:
-			print (read_set_temp() - read_temp())
-			time.sleep(5)
-		
-		print ('Set Temp Reached, Holding for ', dwell_time, ' seconds')
-		time.sleep(dwell_time)
-		
-	set_temp(10)
-
-try:
-	ser = serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=TIMEOUT)
-
-	#request = construct_request(0, 1)
-	#register_address = 0
-	#value_to_write = 2 * 10
-
-	#request = construct_request('read', register_address, 1)
-	#ser.write(request)
-	
-	#response = ser.read(7)
-	#data_bytes = response[3:5]
-	#value = struct.unpack('>h', data_bytes)[0]
-	#setpoint = value / 10.0
-	#print (setpoint)
-	
-	#set_temp(30)
-	#time.sleep(1)
-	#ser.flushInput()
-	#while True:
-	#	print ('current temp is:', read_temp())
-	#	time.sleep(1)
-	temp_ramp_test(3600)
-	ser.close()
-	
-except KeyboardInterrupt:
-	ser.close()
+#except KeyboardInterrupt:
+#	ser.close()
