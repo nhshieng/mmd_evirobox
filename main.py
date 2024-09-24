31e480d3e3d50e94f41f2acfae3c7feb76ce7de1
Shieng Liu <sliu@nhgh.org>
2024-09-24 15:50:19 -0700

    temp and sensor ready for test

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from pid import calculate_crc, construct_request, set_temp, read_set_temp, read_
 import serial
 import struct 
 
+import tkinter as tk
+from tkinter import messagebox
+
 GAS_LIMIT = 1000
 
 ads_1, ads_2 = i2c_setup()
@@ -56,37 +59,36 @@ def check_gas_limit(sensors):
 	
 	return flagged_sensors
 
-
-
 def run_envirbox():
 	try:
 		ser = serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=TIMEOUT)
 		tare_sensors()
-		start = False
-		detect_gas = False
+		heater_start = False
+		gas_detected = False
 
 		# Start up by checking presence of gas before turning on the heater
 		update_sensors(gas_sensors)
 		if check_gas_limit(gas_sensors) is None:
+			heater_start = True
+			gas_detected = False
 			set_temp(TEST_TEMP)
-			start = True
 		else:
 			print(check_gas_limit(gas_sensors), "are over the acceptable limit.")
-			start = False
+			heater_start = False
+			gas_detected = True
 
 		# Monitor temperature and gas sensors and act appropriately
-		while start == True:
+		while heater_start == True:
 			update_sensors(gas_sensors)	
 			print_readings(gas_sensors)
 
 			if check_gas_limit(gas_sensors) is True:
-				deteect_gas = True
+				gas_detected = True
 				set_temp(0)
 			else:
-				if detect_gas is True:
+				if gas_detected is True:
+					gas_detected = False
 					set_temp(TEST_TEMP)
-					detect_gas = False
-				continue
 
 	except KeyboardInterrupt:
 		print("KeyboardInterrupt, exiting...")
@@ -94,6 +96,29 @@ def run_envirbox():
 	finally:	
 		ser.close()
 		print("Serial connection closed.")
+
+def button_click():
+	try:
+		user_input = int(entry.get())
+		messagebox.showinfo("Start", f"You entered:{user_input}")
+	except ValueError:
+		messagebox.showeror("Error", "Please enter a valid integer")
+
+# root = tk.Tk()
+# root.title("Environmental Box")
+
+# # Create and place the input box
+# entry_label = tk.Label(root, text="Set Temp:")
+# entry_label.pack(pady=5)
+# entry = tk.Entry(root)
+# entry.pack(pady=5)
+
+# # Create and place the button
+# button = tk.Button(root, text="Submit", command=button_click)
+# button.pack(pady=20)
+
+# # Run the application
+# root.mainloop()
 
 if __name__ == "__main__":
 	run_envirbox()
